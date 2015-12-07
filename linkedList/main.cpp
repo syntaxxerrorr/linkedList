fc7e32a51f6a557085548058e850b8696dc55aa0
Syntax Error <fahad@talwandi.net>
2015-12-07 16:47:29 +0500

    Initial Commit

---
+++ linkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  linkedList
+//
+//  Created by Fahad Mustafa on 07/12/2015.
+//  Copyright © 2015 Fahad Mustafa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
